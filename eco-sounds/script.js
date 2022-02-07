16d8cc815a436e5cfaeb1478f49d87ac7451a5a6
ReakZ <spec.kov@list.ru>
2022-02-07 13:59:20 +0300

    feat: add change bird functionality

--- eco-sounds/script.js
+++ eco-sounds/script.js
@@ -1,22 +1,40 @@
 let isPlay = false;
-
+let currentBird = "solovey";
 const audio = new Audio();
 audio.src = "./assets/audio/solovey.mp3";
 audio.currentTime = 0;
-
-
 
 const btnPlay = document.querySelector(".player");
 btnPlay.addEventListener("click", playAudio);
 function playAudio() {
   if (!isPlay) {
-    isPlay=true
+    isPlay = true;
     audio.play();
-    btnPlay.classList.toggle("pause")
+    btnPlay.classList.toggle("pause");
   } else {
-    isPlay=false;
+    isPlay = false;
     audio.pause();
-    btnPlay.classList.toggle("pause")
+    btnPlay.classList.toggle("pause");
   }
 }
+const main = document.querySelector(".main");
+const changeBirdsBtns = document.querySelector(".nav");
+changeBirdsBtns.addEventListener("click", changeBird);
+function changeBird(e) {
+  if (e.target.classList.contains("nav__button")) {
+    let btn = document.querySelectorAll(".nav__button");
+    btn.forEach((x) => x.classList.remove("active"));
+    e.target.classList.add("active");
+    main.classList.remove(currentBird);
+    currentBird = e.target.dataset.bird;
+    main.classList.add(currentBird);
+    isPlay = true;
+    audio.src = `./assets/audio/${currentBird}.mp3`;
+    audio.currentTime = 0;
+    audio.play();
 
+    if (!btnPlay.classList.contains("pause")) {
+      btnPlay.classList.add("pause");
+    }
+  }
+}
